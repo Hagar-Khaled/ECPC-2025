91b9a2c61ecb8043f355861441637433e41352d7
Hagar-Khaled <145365849+Hagar-Khaled@users.noreply.github.com>
2025-08-15 22:18:45 +0300

    Add files via upload

---
+++ ECPC Qualifications 2025 Day 1/A.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+void RUN() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+
+int main()
+{
+    RUN();
+    int x,a,b;
+    cin>>x>>a>>b;
+    cout<<(x-(a+b));
+
+    return 0;
+}
